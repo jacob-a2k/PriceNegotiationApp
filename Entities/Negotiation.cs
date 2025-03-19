34241d9c94b1017290e75106de5af43659748e38
Jakub Abramczyk <jakub.a2k@gmail.com>
2025-03-19 09:07:52 +0100

    Create Entities: Product, User,Negotiation

---
+++ Entities/Negotiation.cs
@@ -0,0 +1,14 @@
+﻿namespace PriceNegotiationApp.Entities
+{
+    public class Negotiation
+    {
+        private const int TotalTrys = 3;
+
+        public int Id { get; set; }
+        public Product Item { get; set; }
+        public DateTime BetDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int ActualTry { get; set; }
+        public decimal ActualPrice { get; set; }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace PriceNegotiationApp.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public bool IsLogIn { get; set; }
+        public UserType Type { get; set; }
+    }
+
+    public enum UserType
+    {
+        Client,
+        Employee
+    }
+}
